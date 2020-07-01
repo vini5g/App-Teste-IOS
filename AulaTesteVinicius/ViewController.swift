c392a89ff19e59fefb6d17596d81ce51f56a2faa
vinicius-santos-programmer <vinicius.a.moreira.santos@gmail.com>
2020-07-01 16:59:36 -0300

    primeiros passos

---
+++ AulaTesteVinicius/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  AulaTesteVinicius
+//
+//  Created by COTEMIG on 01/07/20.
+//  Copyright © 2020 Vincius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Linha:\(indexPath.row)"
+        return cell
+    }
+}
+
